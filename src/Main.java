e1c22262e7bbb603b3e3302425f1103c4a39ef71
Andrey P <AndreyP@home>
2023-09-18 16:13:12 +0300

    degree

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println("%d - %d = %d".formatted(a,b,Diff(a,b)));
         System.out.println("%d * %d = %d".formatted(a,b,Prod(a,b)));
         System.out.println("%d / %d = %f".formatted(a,b,Quot(a,b)));
+//////////////////////////////// factoria //////////////////////////////////////////////////////////////////////////
         System.out.print("Введите целое число: ");
         boolean isInt = input.hasNextInt();
         if(isInt){
@@ -21,30 +22,48 @@ public class Main {
         }else{
             System.out.println("Ошибка ввода!");
         }
-
+////////////////////////////////// degree ///////////////////////////////////////////////////////////////////////////////////////////
+        System.out.print("Введите целое число и степень: ");
+        int number = input.nextInt();
+        //System.out.print("Введите степень: ");
+        int degre = input.nextInt();
+        System.out.print("%d ^ %d = %.0f".formatted(number, degre,degree(number, degre)));
     }
+//======================================================
+//================ degree ==============================
+    static double degree(int number, int degre){
+        int sum1 = number;
+        for(int i = 1; i < degre; i++) {
+            sum1 = sum1 * number;
+        }
+        return sum1;
+    }
+//================ factoria ==========================
 
     static int factoria(int n){
         if(n==1)
             return 1;
         return n * factoria(n-1);
-
     }
-
+//=================================================
     static int sum(int a, int b){
         int c = a + b;
         return c;
     }
+//==================================================
     static int Diff(int a, int b){
         return a - b;
     }
+//====================================================
     static int Prod(int a, int b){
         //Product - Произведение
         return a * b;
     }
+//========================================================
     static double Quot(int a, int b){
         // Quotient - частное
         return (double)a / b;
     }
+//===========================================================
 
 }
